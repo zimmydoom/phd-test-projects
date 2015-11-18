fb08ec6064516a9c2d55df7dd0bb805bf291c384
David Mendez Lopez <zimmydoom@iMac.local>
2015-11-18 00:05:31 -0600

    added Make_winner_list, corrected while loop in generate_execution

--- distirbuted-local-matchmaking/offline-matchmaking/DistributedExecutionGenerator.py
+++ distirbuted-local-matchmaking/offline-matchmaking/DistributedExecutionGenerator.py
@@ -6,6 +6,7 @@ Created on Oct 28, 2015
 
 import networkx as nx
 import random
+from numpy.random import choice as nrandom
 
 
 
@@ -19,26 +20,42 @@ def read_dataset(self):
 
 
 
-def generate_execution(self,comcycles,rtt_matrix, nodelist, cycle_length):
+def generate_execution(self,comcycles,rtt_matrix, nodelist):
     """ takes a list of nodes on a network and generates 
        a list of tuples  (a,b) representing a message between node a and node b
        an empty tuple represents a decision point.
        There are comcycles = decision points.
     """
     execution_list = []
-    cyclen = cycle_length
     while comcycles > 0:
         for node in nodelist:
             cycle_counter = 0
-            while cycle_counter <= cyclen:
+            how_many_messages = random.choice(nodelist)
+            while cycle_counter <= how_many_messages:
                 receiver =  random.choice(nodelist)
                 if receiver == node:
                     receiver = random.choice(nodelist)
                 execution_list.append((node[0],receiver[0]))
-                cycle_counter += rtt_matrix[node[0]][receiver[0]] #simular el tiempo que pasa llegando mensajes
+                #cycle_counter += rtt_matrix[node[0]][receiver[0]] #simular el tiempo que pasa llegando mensajes
                 cycle_counter += 1
         execution_list.append(())
+        execution_list.append(self.make_winner_list(len(nodelist)))
         comcycles -= 1
         
     return execution_list
 
+
+def make_winner_list(self,size):
+    """out of a list of size size selects a random number of them with a probability (num + 1)/ (size + 5)
+    
+    """
+    winner_list = []
+    numbers = range(1,size)
+    for num in numbers:
+        p = (num + 1)/ (size + 5)
+        ap = 1 - p
+        coin = nrandom([0,1],[ap,p])
+        if coin == 1:
+            winner_list.append(num)
+    return winner_list
+    
